2276e5353f594b5c2c756ed5bb9a8f91225c45cf
visethhul <visethhul>
2024-04-16 07:54:23 +0000

    add new page talking to api

--- pages/Page1/jsobjects/data/data.js
+++ pages/Page1/jsobjects/data/data.js
@@ -1,5 +1,6 @@
 export default {
 	todos: undefined,
+	activeTodo:undefined,
 	dummyTodos:[
 		{title:'use the the input to add a todo',completed:true},
 		{title:'use the the input to add a todo',completed:false},
